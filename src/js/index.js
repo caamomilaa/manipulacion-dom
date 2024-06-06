20f8875d8c8ee332d723778b536749e5a667b90c
caamomilaa <c.velazquezdamico@gmail.com>
2024-06-06 22:03:08 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -33,3 +33,25 @@ button.addEventListener("click", countdownEvent);
 //Elabore un programa con javascript que en el navegador permita a través de un input en el que se introducen una lista de números separados por coma y del click de un botón sumar toda la lista de números introducidos.
 
 //lsiatde numeros separados por comas en el campo de input, y emterlos dentro de un acummluador para sumar. inpunto da srting no number, hay que transformar. Coger el campo que entra. partir el sring (la coma seria separador7 split), devolveria el arreglo con la lista de elementos, pero esa lista son cadena de texto, posterior iterarlos y cada ve que entre en algunos de indice, cambiar a numero. El for..in serviría.
+
+//function addNumbersinput
+
+//EJERCICIO: Tabla de multiplicar
+//Elabore un programa con javascript que en el navegador permita a través la lectura de un número x que se escriba en el html y el click de un botón que permita generar la tabla de multiplicar de ese número (x del 1 al 10)
+
+const lecturaNumero = document.getElementById("multiplication");
+const multiplicador = document.getElementById("multiplicationButton");
+
+function multiplicationEvent() {
+  for (let i = 1; i <= 10; i++) {
+    document.createElement("p");
+    const parrafo = document.createElement("p");
+    parrafo.textContent = lecturaNumero.value * i;
+    document.body.appendChild(parrafo);
+    console.log(lecturaNumero.value * i);
+  }
+}
+
+multiplicador.addEventListener("click", multiplicationEvent);
+
+//textcontet -> cadena de texto
